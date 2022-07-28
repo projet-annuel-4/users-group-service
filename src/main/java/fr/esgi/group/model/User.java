aff18ed59ed80d785cdc4488240d59013280fccd
Haithem LIMAM <heithem.limame@gmail.com>
2022-07-28 02:51:40 +0200

    Fix errors

--- src/main/java/fr/esgi/group/model/User.java
+++ src/main/java/fr/esgi/group/model/User.java
@@ -10,8 +10,6 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
-    @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq", initialValue = 1, allocationSize = 1)
     private Long id;
     private String email;
     private String password;
